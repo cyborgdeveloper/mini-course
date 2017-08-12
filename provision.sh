29a8093632300d8c7d5135e91933a5ec91d5d106
Cyborg Develper <rcmoutinho@cyborgdeveloper.tech>
2017-08-12 17:36:20 -0300

    download, package and deploy a simple maven app

--- provision.sh
+++ provision.sh
@@ -2,3 +2,10 @@ apt-get update -y
 apt-get install default-jdk -y
 apt-get install tomcat7 -y
 apt-get install maven -y
+
+cd /vagrant/
+wget https://github.com/cyborgdeveloper/maven-web/archive/master.zip
+unzip master.zip
+cd maven-web-master/
+mvn package
+cp /vagrant/maven-web-master/target/maven-web.war /var/lib/tomcat7/webapps/
